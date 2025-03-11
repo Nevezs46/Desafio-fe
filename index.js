407eb8afaba5be4463d9025e096ec19cd9b61783
leonardo neves <leonardotheoz12@gmail.com>
2025-03-10 22:58:32 -0300

    estudandooCodigo

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ for(let cont = 0; cont < nomes.length; cont++){
 }
 
 
-
+let te = niveis[3]
 nivel = nomes[5]
 
 if(nivel == nomes[0]){
-    console.log("nivel atualizado " + nomes[0])
+    console.log("nivel atualizado " + niveis[0])
 
 } else if(nivel == nomes[1]){
-    console.log("nivel atualizado " + nomes[1])
+    console.log("nivel atualizado " + niveis[1])
 
 } else if(nivel == nomes[2]){
-    console.log("nivel atualizado " + nomes[2])
+    console.log("nivel atualizado " + niveis[2])
 
 } else if(nivel == nomes[3]){
-    console.log("nivel atualizado " + nomes[3])
+    console.log("nivel atualizado " + niveis[3])
     
 } else if(nivel == nomes[4]){
-    console.log("nivel atualizado " + nomes[4])
+    console.log("nivel atualizado " + niveis[4])
 
 } else if(nivel == nomes[5]){
-    console.log("nivel atualizado " + nomes[5])
+    console.log("nivel atualizado " + niveis[5])
 
 } else if(nivel == nomes[6]){
-    console.log("nivel atualizado " + nomes[6])
+    console.log("nivel atualizado " + niveis[6])
 
 } else if(nivel == nomes[7]){
-    console.log("nivel atualizado " + nomes[7])
+    console.log("nivel atualizado " + niveis[7])
 } else{
     console.log("desculpa niveis nao localizado")
 }
